496a8205a1a0db71c23b9b081c3f3f61d6cdabbc
Vincent Nguyen <vpvnguyen@gmail.com>
2019-12-06 09:34:44 -0800

    inital express server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('here');
+});
+
+app.listen(PORT, () => {
+    console.log(PORT)
+});
